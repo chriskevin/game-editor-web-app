a1b31fe1e972c86702fa80d51c546f1fc2163e20
unknown <CHSU7648@WL08721.sigma.local>
2016-04-25 23:05:35 +0200

    Some initial config

---
+++ entry.js
@@ -0,0 +1,8 @@
+require("./bower_components/foundation-sites/dist/foundation-flex.css");
+require("./style.css");
+document.write("It works.");
+/*new webpack.DefinePlugin({
+  "process.env": {
+    NODE_ENV: JSON.stringify("production")
+  }
+});*/
